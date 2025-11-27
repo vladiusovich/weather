e5dab5a8504307c6f7eddf9f1c0b2217bec5d915
vladiusovich <vladiusovich@gmail.com>
2025-11-28 03:38:13 +0400

    fix:  bad bg color when "back" navigation works

--- src/theme/BackgroundUpdateProvider.tsx
+++ src/theme/BackgroundUpdateProvider.tsx
@@ -1,14 +1,13 @@
 import React, { useEffect } from "react";
-import { useTheme } from "tamagui";
 import * as SystemUI from "expo-system-ui";
 
 // Init global backgound color
 const BackgroundUpdateProvider: React.FC = () => {
-    const theme = useTheme();
-
     useEffect(() => {
-        SystemUI.setBackgroundColorAsync(theme.background04.val ?? "background04");
-    }, [theme.background04.val]);
+        // Use a transparent color because when "navigation.back" is launched (by the system or an application),
+        // the screen will flash some color.
+        SystemUI.setBackgroundColorAsync("transparent");
+    }, []);
 
     return null;
 };
